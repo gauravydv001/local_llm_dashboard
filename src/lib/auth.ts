0d432b1667e0f442de5b6c2bc065b14173c424fd
gaurav <91313220+gauravydv001@users.noreply.github.com>
2026-05-14 15:11:43 +0530

    Change token.id assignment to use user.email

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,7 +17,7 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = token.id || uuidv4();
+        token.id = token.id || user.email;
         token.email = user.email;
         token.image = user.image;
       }
